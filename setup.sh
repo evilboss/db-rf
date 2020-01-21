a1ff3473fb100d7f4e2cd2057cbe6028f227a1a5
Jr Reyes <jr.evilboss@gmail.com>
2020-01-21 21:18:11 +0800

    update setup to setup frontend

--- setup.sh
+++ setup.sh
@@ -4,7 +4,9 @@ git submodule init
 git submodule sync
 git submodule update --remote
 cd backend
+
 pwd
+
 echo "Starting setting up server"
 
 pipenv install
@@ -13,3 +15,8 @@ pipenv install
 python manage.py migrate
 echo "Setup super user"
 python manage.py migrate
+python manage.py migrate
+echo "Setup React client"
+cd react_frontend
+yarn && yarn build
+echo "Setup Complete run via ./run"
